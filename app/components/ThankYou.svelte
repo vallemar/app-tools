a336c1af441c85611d18d4ad4905bc15981e2e04
Martin Guillon <dev@akylas.fr>
2025-01-06 15:37:07 +0100

    chore: in app purchase feature

---
+++ app/components/ThankYou.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { lc } from '~/helpers/locale';
+</script>
+
+<gesturerootview rows="auto">
+    <lottie async={true} autoPlay={true} height={400} loop={false} src="~/assets/lottie/firework.lottie" width="100%" />
+    <label horizontalAlignment="center" textAlignment="center" textWrap={true} verticalAlignment="center" width="80%">
+        <span fontSize={42} fontWeight="bold" text={lc('thank_you')} />
+        <span fontSize={22} text={'\n\n' + lc('thank_you_subtitle')} />
+    </label>
+</gesturerootview>
--- app/utils/inapp-purchase.ts
+++ app/utils/inapp-purchase.ts
@@ -1,7 +1,10 @@
 import { BuyItemOptions, Item as InAppItem, InAppPurchase, PaymentEvent } from '@akylas/nativescript-inapp-purchase/index';
+import { showBottomSheet } from '@nativescript-community/ui-material-bottomsheet/svelte';
+import { showError } from './showError';
+import { lc } from '@nativescript-community/l';
 
 let inappPurchase: InAppPurchase;
-let inappItems: InAppItem[];
+export let inappItems: InAppItem[];
 export function init() {
     if (!inappPurchase) {
         inappPurchase = new InAppPurchase();
@@ -13,39 +16,45 @@ export function init() {
                         const canPay = inappPurchase.canMakePayments();
                         if (canPay) {
                             // pass in your product IDs here that you want to query for
-                            inappPurchase.fetchItems(['io.nstudio.iapdemo.coinsfive', 'io.nstudio.iapdemo.coinsone', 'io.nstudio.iapdemo.coinsonethousand']);
+                            inappPurchase.fetchItems([__APP_ID__ + '.smalltip', __APP_ID__ + '.mediumtip', __APP_ID__ + '.largetip', __APP_ID__ + '.largertip', __APP_ID__ + '.bigtip']);
                         }
+                    } else if (event.result === PaymentEvent.Result.FAILURE) {
+                        DEV_LOG && console.log('in_app_failure', (event as PaymentEvent.ConnectingStore.IFailure).payload.description);
+
+                        // showError(new Error(lc('in_app_failure', (event as PaymentEvent.ConnectingStore.IFailure).payload.description)));
                     }
                     break;
                 case PaymentEvent.Context.RETRIEVING_ITEMS:
                     if (event.result === PaymentEvent.Result.SUCCESS) {
                         // if you passed multiple items you will need to handle accordingly for your app
                         inappItems = (event as PaymentEvent.RetrievingItems.ISuccess).payload;
+                        DEV_LOG && console.log('inappItems', inappItems);
+                    } else if (event.result === PaymentEvent.Result.FAILURE) {
+                        DEV_LOG && console.log('in_app_failure', (event as PaymentEvent.RetrievingItems.IFailure).payload.description);
+                        // showError(new Error(lc('in_app_failure', (event as PaymentEvent.RetrievingItems.IFailure).payload.description)));
                     }
                     break;
                 case PaymentEvent.Context.PROCESSING_ORDER:
                     if (event.result === PaymentEvent.Result.FAILURE) {
-                        console.log(`🛑 Payment Failure - ${(event as PaymentEvent.ProcessingOrder.IFailure).payload.description} 🛑`);
+                        DEV_LOG && console.log('in_app_failure', (event as PaymentEvent.ProcessingOrder.IFailure).payload.description);
+                        // showError(new Error(lc('in_app_failure', (event as PaymentEvent.ProcessingOrder.IFailure).payload.description)));
                         // handle the failure of the purchase
                     } else if (event.result === PaymentEvent.Result.SUCCESS) {
                         const payload = (event as PaymentEvent.ProcessingOrder.ISuccess).payload;
                         // handle the successful purchase
-                        console.log('🟢 Payment Success 🟢');
-                        console.log(`Order Date: ${payload.orderDate}`);
-                        console.log(`Receipt Token: ${payload.receiptToken}`);
                         inappPurchase.finalizeOrder(payload);
                     }
                     break;
                 case PaymentEvent.Context.FINALIZING_ORDER:
                     if (event.result === PaymentEvent.Result.SUCCESS) {
-                        console.log('Order Finalized');
+                        showThankYou();
+                    } else if (event.result === PaymentEvent.Result.FAILURE) {
+                        showError(new Error(lc('in_app_failure', (event as PaymentEvent.FinalizingOrder.IFailure).payload.description)));
                     }
                     break;
                 case PaymentEvent.Context.RESTORING_ORDERS:
-                    console.log(event);
                     break;
                 default:
-                    console.log(`Invalid EventContext: ${event}`);
                     break;
             }
         });
@@ -54,19 +63,62 @@ export function init() {
     return inappPurchase;
 }
 
-export function buySponsor() {
+async function showThankYou() {
+    const ThankYou = (await import('@shared/components/ThankYou.svelte')).default;
+    return showBottomSheet({
+        parent: null,
+        skipCollapsedState: true,
+        view: ThankYou,
+        ignoreTopSafeArea: true,
+        props: {}
+    });
+}
+
+export async function presentInAppSponsorBottomsheet() {
+    const OptionSelect = (await import('~/components/common/OptionSelect.svelte')).default;
+    const options = inappItems
+        .sort((a, b) => a.priceAmount - b.priceAmount)
+        .map((i) => {
+            const id = i.itemId.split('.').pop();
+            return {
+                data: i,
+                title: lc(`inapp.${id}`),
+                subtitle: lc(`inapp.${id}_desc`),
+                rightValue: i.priceFormatted
+            };
+        });
+    const rowHeight = 80;
+    const height = Math.min(rowHeight * options.length, 400);
+    const result = await showBottomSheet({
+        parent: null,
+        view: OptionSelect,
+        peekHeight: 400,
+        ignoreTopSafeArea: true,
+        props: {
+            autoSizeListItem: true,
+            options,
+            rowHeight,
+            height
+        }
+    });
+    if (result) {
+        buySponsorItem(result.data);
+    }
+}
+
+export function buySponsorItem(item: InAppItem) {
     const opts: BuyItemOptions = {
-        accountUserName: 'someuseraccount123@test.orgbizfree',
+        // accountUserName: 'someuseraccount123@test.orgbizfree',
         android: {
             // vrPurchase: true
         },
         ios: {
-            quantity: 1,
-            simulatesAskToBuyInSandbox: true
+            quantity: 1
+            // simulatesAskToBuyInSandbox: true
         }
     };
 
     // This method will kick off the platform purchase flow
     // We are passing the item and an optional object with some configuration
-    inappPurchase.buyItem(inappItems, opts);
+    inappPurchase.buyItem(item, opts);
 }
