3ef5d468c55afc76b39b6e3235d1c559495e3983
farfromrefug <dev@akylas.fr>
2025-01-16 15:40:00 +0100

    chore: tupings

--- app/typings/references.d.ts
+++ app/typings/references.d.ts
@@ -1,10 +1,11 @@
-/// <reference path="./../../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!Foundation.d.ts" />
-/// <reference path="./../../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!CoreLocation.d.ts" />
-/// <reference path="./../../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!CoreFoundation.d.ts" />
-/// <reference path="./../../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!ObjectiveC.d.ts" />
-/// <reference path="./../../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!UIKit.d.ts" />
+/// <reference path="../../../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!Foundation.d.ts" />
+/// <reference path="../../../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!CoreLocation.d.ts" />
+/// <reference path="../../../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!CoreFoundation.d.ts" />
+/// <reference path="../../../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!ObjectiveC.d.ts" />
+/// <reference path="../../../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!UIKit.d.ts" />
 /// <reference path="../../../node_modules/@nativescript/types-android/lib/android-34.d.ts" />
 /// <reference path="../../../node_modules/@nativescript/core/global-types.d.ts" />
+/// <reference path="../node_modules/@nativescript/types-ios/lib/ios/interop.d.ts" />
 
 declare const __APP_ID__: string;
 declare const __APP_VERSION__: string;
@@ -20,6 +21,11 @@ declare const PRODUCTION: boolean;
 declare const SENTRY_ENABLED: boolean;
 declare const SENTRY_DSN: string;
 declare const SENTRY_PREFIX: string;
+declare const PLAY_STORE_BUILD: boolean;
+declare const GIT_URL: string;
+declare const STORE_LINK: string;
+declare const STORE_REVIEW_LINK: string;
+declare const SPONSOR_URL: string;
 
 declare const SUPPORTED_LOCALES: string[];
 declare const SUPPORTED_COLOR_THEMES: string[];
